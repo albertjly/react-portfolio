a67f8dd10161696778b698693cd6d94c561750f4
Albert Jiang <albertjly@gmail.com>
2021-07-04 19:24:38 -0700

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import ContactForm from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
+  const [categories] = useState([
+    { name: 'portfolio' },
+    { name: 'resume' },
+    { name: 'contact' },
+    { name: 'about me' }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
+      <main>
+        {currentCategory.name === "about me" && <About />}
+        {currentCategory.name === "contact" && <ContactForm />}
+        {currentCategory.name === "portfolio" && <Portfolio />}
+        {currentCategory.name === "resume" && <Resume />}
+      </main>
+      <Footer />
     </div>
   );
 }
